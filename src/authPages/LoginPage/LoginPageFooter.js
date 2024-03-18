e185b783d55858b4bd72eafdbe718a9f240170a5
Abdullah Abu Tasneem <abdullahabutasneem19@gmail.com>
2024-03-19 03:56:48 +0600

    Creating Tooltip message on Mouse over on Log in button

--- src/authPages/LoginPage/LoginPageFooter.js
+++ src/authPages/LoginPage/LoginPageFooter.js
@@ -2,6 +2,15 @@ import React from 'react';
 import CustomPrimaryButton from '../../shared/components/CustomPrimaryButton';
 import RedirectInfo from '../../shared/components/RedirectInfo';
 import { useNavigate } from 'react-router-dom';
+import { Tooltip } from '@mui/material';
+
+const getFormNotValidMessage = () => {
+    return "Enter correct e-mail address and password should contains between 6 and 12 characters"
+}
+
+const getFormValidMessage = () => {
+    return "Press to Log in!"
+}
 
 const LoginPageFooter = ({ handleLogin, isFormValid }) => {
     const navigate = useNavigate();
@@ -11,15 +20,18 @@ const LoginPageFooter = ({ handleLogin, isFormValid }) => {
     
     return (
         <>
-            <div>
-                <CustomPrimaryButton
-                    label="Log in"
-                    additionalStyles={{ marginTop: '30px' }}
-                    disabled={!isFormValid}
-                    onClick={handleLogin}
-                >
-                </CustomPrimaryButton>
-            </div>
+            <Tooltip
+                title={!isFormValid ? getFormNotValidMessage() : getFormValidMessage()}>
+                <div>
+                    <CustomPrimaryButton
+                        label="Log in"
+                        additionalStyles={{ marginTop: '30px' }}
+                        disabled={!isFormValid}
+                        onClick={handleLogin}
+                    >
+                    </CustomPrimaryButton>
+                </div>
+            </Tooltip>
             <RedirectInfo 
                 text='Need an account?  '
                 redirectText='Create an account'
