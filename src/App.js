1cdb0bfb4353cb7ec3b14e978c1a5d228b83e09e
Abdullah Abu Tasneem <abdullahabutasneem19@gmail.com>
2024-03-19 01:02:16 +0600

    Adding routing to App

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import './App.css';
+import RegisterPage from './authPages/RegisterPage/RegisterPage';
+import LoginPage from './authPages/LoginPage/LoginPage';
+import Dashboard from './Dashboard/Dashboard';
 
 function App() {
   return (
-    <div className="App">
-      Hello Tasneem
-    </div>
+    <>
+      <Router>
+        <Routes>
+          <Route exact path="/login" element={
+              <LoginPage></LoginPage>
+            }>   
+          </Route>
+          <Route exact path="/register" element={
+              <RegisterPage></RegisterPage>
+            }>   
+          </Route>
+          <Route exact path="/dashboard" element={
+              <Dashboard></Dashboard>
+            }>   
+          </Route>
+          <Route path="/" element={
+              <Navigate to="/dashboard"></Navigate>
+            }>
+          </Route>
+        </Routes>
+      </Router>
+    </>
   );
 }
 
